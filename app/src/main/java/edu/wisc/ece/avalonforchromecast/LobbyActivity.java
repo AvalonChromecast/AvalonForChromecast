ea2bc5b1c135ffddadf3057f88d52f7929a57bb1
wontonii232 <cudzinovic@wisc.edu>
2016-10-26 17:45:47 -0500

    creating gameLogic again

--- app/src/main/java/edu/wisc/ece/avalonforchromecast/LobbyActivity.java
+++ app/src/main/java/edu/wisc/ece/avalonforchromecast/LobbyActivity.java
@@ -2,6 +2,7 @@ package edu.wisc.ece.avalonforchromecast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LobbyActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class LobbyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+    }
+
+    /* called when pressing the start game button */
+    public void startGame(View view){
+        // Send signal to chromecast to start the game
+
+        // The chromecast should then signal all devices connected to it that the game is starting
+        // When devices receive this signal they should move to the game activity
     }
 }
