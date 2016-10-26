07beea504d176b047ed6ed14e424104ccdbaaa9a
wontonii232 <cudzinovic@wisc.edu>
2016-10-26 17:02:59 -0500

    Initial game logic

--- app/src/main/java/edu/wisc/ece/avalonforchromecast/MainActivity.java
+++ app/src/main/java/edu/wisc/ece/avalonforchromecast/MainActivity.java
@@ -2,6 +2,7 @@ package edu.wisc.ece.avalonforchromecast;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /* called when user clicks connect button */
+    public void connectToCast(View view){
+        // connect user to chromecast
+
+        // move user to welcome screen
     }
 }
--- app/src/main/java/edu/wisc/ece/avalonforchromecast/WelcomeActivity.java
+++ app/src/main/java/edu/wisc/ece/avalonforchromecast/WelcomeActivity.java
@@ -2,6 +2,7 @@ package edu.wisc.ece.avalonforchromecast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    /* adds player to the lobby */
+    public void joinLobby(View view){
+        // if the user entered a name, add them to the lobby
+
+        // chromecast should update ui by adding player's name to list on the big screen
+
+        // move to lobby ui
     }
 }
