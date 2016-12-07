c3dc93a67e923446cde725b463d6e800b927b39a
wontonii232 <cudzinovic@wisc.edu>
2016-12-07 15:20:02 -0600

    Initialized Setup Leader

--- app/src/main/java/edu/wisc/ece/avalonforchromecast/MainActivity.java
+++ app/src/main/java/edu/wisc/ece/avalonforchromecast/MainActivity.java
@@ -69,6 +69,8 @@ public class MainActivity extends AppCompatActivity implements Observer {
         mSetupFragment = new SetupFragment();
         mGameOverFragment = new GameOverFragment();
 
+        mSetupLeader = false;
+
         updateFragments();
     }
 
